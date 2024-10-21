e0eaf85851a4ec2f09d6405185b1ab8c248d1469
YerzatCode <yerzat.nopazyl@gmail.com>
2024-10-21 20:34:53 +0500

    авы

--- Assets/Script/Main.cs
+++ Assets/Script/Main.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Main : MonoBehaviour
@@ -5,7 +7,7 @@ public class Main : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        System.Console.WriteLine("Hello World!");
     }
 
     // Update is called once per frame
